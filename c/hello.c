0d58ff2c2b797881588b11cca8cb6bc77880721b
riswanmsm <msmr1986@gmail.com>
2023-02-10 04:25:22 +0530

    c practice

---
+++ c/hello.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int i = 5;
+    char ch = 'a';
+    float f = 1.234;
+    double d = 1.23456789999999898;
+
+    printf("the value of float: %f", f);
+    printf("The value of double: %0.5f", d);
+    printf("Hello, World!\n");
+    printf("Hello, World!\n");
+    printf("The value of i: %d\n", i);
+    printf("The value of ch: %c", ch);
+    return 0;
+}
